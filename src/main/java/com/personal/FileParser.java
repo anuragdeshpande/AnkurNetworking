95e25cdd919912620026f0e36ea71aefaf4f2c5f
Anurag Deshpande <adeshpande@idfbins.com>
2021-01-30 10:32:12 -0700

    finishing task 2 and 3

--- src/main/java/com/personal/FileParser.java
+++ src/main/java/com/personal/FileParser.java
@@ -13,6 +13,8 @@ import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import de.vandermeer.asciitable.AsciiTable;
 import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
+import org.apache.commons.lang3.ArrayUtils;
+import sun.security.util.ArrayUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,18 +27,18 @@ public class FileParser {
     public static void main(String[] args) throws IOException {
         JavaSymbolSolver solver = new JavaSymbolSolver(new CombinedTypeSolver());
         StaticJavaParser.getConfiguration().setSymbolResolver(solver);
-        CompilationUnit compilationUnit = StaticJavaParser.parse(new File("./src/main/java/com/personal/Person.java"));
+        CompilationUnit compilationUnit = StaticJavaParser.parse(new File("./src/main/java/com/personal/JFrameExample.java"));
         ClassOrInterfaceDeclaration sourceFile = compilationUnit.getTypes().get(0).asClassOrInterfaceDeclaration();
         List<MethodDeclaration> methods = sourceFile.getMethods();
         /*Task 1
-        * For each given method find
-        * 1. name of the method
-        * 2. return type
-        * 3. start line
-        * 4. end line
-        * 5. parameter type (if applicable)
-        * 6. parameter name (if applicable)
-        * */
+         * For each given method find
+         * 1. name of the method
+         * 2. return type
+         * 3. start line
+         * 4. end line
+         * 5. parameter type (if applicable)
+         * 6. parameter name (if applicable)
+         * */
 //        AsciiTable task1 = new AsciiTable();
 //        task1.addRule();
 //        task1.addRow(null, null, null, null, null, "Task 1: Methods list and their details").setTextAlignment(TextAlignment.CENTER);
@@ -66,47 +68,98 @@ public class FileParser {
 
 
         /*Task 2
-        * For each line in a method body, if it is a function call
-        * find other methods that have been called on the variable
-        */
-        HashMap<String, ArrayList<Expression>> methodCalls = new HashMap<>();
+         * For each line in a method body, if it is a function call
+         * find other methods that have been called on the variable
+         */
+        HashMap<String, ArrayList<String>> methodCalls = new HashMap<>();
         methods.forEach(method -> {
             method.getBody().ifPresent(body -> {
                 for (Statement statement : body.getStatements()) {
                     statement.ifExpressionStmt(expressionStmt -> {
                         expressionStmt.getExpression().ifVariableDeclarationExpr(variableDeclarationExpr -> {
                             String nameAsString = variableDeclarationExpr.getVariables().get(0).getNameAsString();
-                            if(methodCalls.containsKey(nameAsString)){
-                                methodCalls.get(nameAsString).add(variableDeclarationExpr);
+                            if (methodCalls.containsKey(nameAsString)) {
+                                methodCalls.get(nameAsString).add(variableDeclarationExpr.getVariables().get(0).getType().asString());
                             } else {
-                                ArrayList<Expression> objects = new ArrayList<>();
-                                objects.add(variableDeclarationExpr);
+                                ArrayList<String> objects = new ArrayList<>();
+                                objects.add(variableDeclarationExpr.getVariables().get(0).getType().asString());
                                 methodCalls.put(nameAsString, objects);
                             }
+                            Object[] array = methodCalls.get(nameAsString).toArray();
+                            ArrayUtils.reverse(array);
+//                            System.out.println("Found method call for : " + nameAsString + ", List until now: " + Arrays.toString(array));
+
                         });
 
                         expressionStmt.getExpression().ifMethodCallExpr(methodCallExpr -> {
                             methodCallExpr.getScope().ifPresent(expression -> {
-                                expression.ifNameExpr(nameExpr -> {
-                                    if(methodCalls.containsKey(nameExpr.getNameAsString())){
-                                        methodCalls.get(nameExpr.getNameAsString()).add(methodCallExpr);
-                                    } else {
-                                        ArrayList<Expression> objects = new ArrayList<>();
-                                        methodCalls.put(nameExpr.getNameAsString(), objects);
-                                    }
-                                });
+                                resolveMethodCalls(methodCallExpr, methodCalls);
                             });
                         });
                     });
                 }
             });
         });
-        methodCalls.forEach((key, value) -> {
-            System.out.println(key);
-            System.out.println(Arrays.toString(value.toArray()));
+
+        /*
+         * Task 3: For each receiver variable collect all the methods that take this variable as a parameter.
+         */
+
+        methods.forEach(method -> {
+            method.getBody().ifPresent(body -> {
+                for (Statement statement : body.getStatements()) {
+                    statement.ifExpressionStmt(expressionStmt -> {
+                        expressionStmt.getExpression().ifVariableDeclarationExpr(variableDeclarationExpr -> {
+                            variableDeclarationExpr.getVariables().get(0).getInitializer().ifPresent(expression -> {
+                                expression.ifObjectCreationExpr(objectCreationExpr -> {
+                                    objectCreationExpr.getArguments().forEach(argument -> {
+                                        if(methodCalls.containsKey(argument.toString())){
+                                            int line = objectCreationExpr.getBegin().get().line;
+                                            System.out.println(argument+" was used as parameter while creating object for "+objectCreationExpr.getType()+" on line: "+line);
+                                        }
+                                    });
+                                });
+                            });
+
+                        });
+                        expressionStmt.getExpression().ifMethodCallExpr(methodCallExpr -> {
+                            methodCallExpr.getArguments().forEach(argument -> {
+                                if(methodCalls.containsKey(argument.toString())){
+                                    int line = methodCallExpr.getBegin().get().line;
+                                    System.out.println(argument+" was used as a parameter in method call "+methodCallExpr.getNameAsString()+" on line: "+line);
+                                }
+                            });
+                        });
+                    });
+                }
+            });
         });
+    }
 
+    private static void resolveMethodCalls(MethodCallExpr methodCallExpr, HashMap<String, ArrayList<String>> methodCalls) {
+        Expression expression = null;
+        if (methodCallExpr.getScope().isPresent()) {
+            expression = methodCallExpr.getScope().get();
+        }
 
+        if (expression != null) {
+            expression.ifNameExpr(nameExpr -> {
+                if (methodCalls.containsKey(nameExpr.getNameAsString())) {
+                    methodCalls.get(nameExpr.getNameAsString()).add(methodCallExpr.getNameAsString());
+                } else {
+                    ArrayList<String> objects = new ArrayList<>();
+                    objects.add(methodCallExpr.getNameAsString());
+                    methodCalls.put(nameExpr.getNameAsString(), objects);
+                }
+                Object[] array = methodCalls.get(nameExpr.getNameAsString()).toArray();
+                ArrayUtils.reverse(array);
+//                System.out.println("Found method call for : " + nameExpr.getNameAsString() + ", List until now: " + Arrays.toString((array)));
 
+            });
+
+            if (expression.isMethodCallExpr()) {
+                resolveMethodCalls(expression.asMethodCallExpr(), methodCalls);
+            }
+        }
     }
 }
